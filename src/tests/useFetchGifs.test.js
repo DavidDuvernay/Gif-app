ed405316ba9e24f29a297a394ef12cc1db25b57a
David Duvernay <david.anderson0612@gmail.com>
2021-02-15 21:13:10 -0500

    Testing custom hook

---
+++ src/tests/useFetchGifs.test.js
@@ -0,0 +1,16 @@
+const { useFetchGifs } = require("../hooks/useFetchGifs");
+const { renderHook } = require("@testing-library/react-hooks");
+
+
+describe('Pruebas en el hook useFetchGifs', () => {
+
+    test('Debe de retornar el estado inicial',() => {
+
+        const { result } = renderHook( () => useFetchGifs( 'One Punch' ) )
+        const { data, loading } = result.current;
+
+        expect( data ).toEqual( [] );
+        expect( loading ).toBe( true );
+
+    });
+});
